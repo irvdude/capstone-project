647510c9bb8f2a8bdf8ce6792b6f10a72b3da72d
Irvin Dinkins <irvdude44@gmail.com>
2023-10-03 18:56:34 -0400

    added first few models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,14 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"Username: {self.username}"
