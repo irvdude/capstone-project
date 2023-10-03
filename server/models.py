6c15eced24aa910df1e75294889be853ab7fe03e
Irvin Dinkins <irvdude44@gmail.com>
2023-10-03 19:36:04 -0400

    fcompleted Ticket and Comment model

--- server/models.py
+++ server/models.py
@@ -15,5 +15,24 @@ class User(db.Model, SerializerMixin):
     admin = db.Column(db.Boolean, default=False)
     _password_hash = db.Column(db.String)
 
-    def __repr__(self):
-        return f"Username: {self.username}"
+    comments = db.relationship("Comment", backref="user")
+
+
+class Ticket(db.Model, SerializerMixin):
+    __tablename__ = "tickets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(255), nullable=False)  # at least 5 chars
+    status = db.Column(db.String, default="Opened")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255))
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
