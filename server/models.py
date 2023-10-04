f0c5b48cb0df00694e76376e5374748e713818f8
Irvin Dinkins <irvdude44@gmail.com>
2023-10-04 17:57:52 -0400

    added enum to add enumeration to status to ticket

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
+from enum import Enum
 
 # Models go here!
 
@@ -18,6 +19,14 @@ class User(db.Model, SerializerMixin):
     comments = db.relationship("Comment", backref="user")
     roles = db.relationship("Role", secondary="role_identifier")
 
+    def __repr__(self):
+        return f"Username: {self.username}, Admin?: {self.admin}"
+
+
+class TicketStatus(Enum):
+    OPENED = "Opened"
+    RESOLVED = "Resolved"
+
 
 class Ticket(db.Model, SerializerMixin):
     __tablename__ = "tickets"
@@ -25,9 +34,14 @@ class Ticket(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(255), nullable=False)  # at least 5 chars
-    status = db.Column(db.String, default="Opened")  # either opened or resolved
+    status = db.Column(
+        db.Enum(TicketStatus), default=TicketStatus.OPENED
+    )  # either opened or resolved
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"Title {self.title}, Status: {self.status}, Created: {self.created_at}"
 
 
 class Comment(db.Model, SerializerMixin):
@@ -38,6 +52,9 @@ class Comment(db.Model, SerializerMixin):
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
+    def __repr__(self):
+        return f"body: {self.body}"
+
 
 class Role(db.Model, SerializerMixin):
     __tablename__ = "roles"
@@ -45,9 +62,12 @@ class Role(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(10))  # either Admin or Associate
 
+    def __repr__(self):
+        return f"role: {self.role}"
+
 
 role_identifier = db.Table(
     "role_identifier",
     db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
-    db.Column("role_id", db.Intger, db.ForeignKey("roles.id")),
+    db.Column("role_id", db.Integer, db.ForeignKey("roles.id")),
 )
