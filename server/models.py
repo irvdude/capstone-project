c3dcadd317f6e02afdba1912bec4747b20c1e8d6
Irvin Dinkins <irvdude44@gmail.com>
2023-10-03 20:08:51 -0400

    added many-to-many implemtation

--- server/models.py
+++ server/models.py
@@ -16,6 +16,7 @@ class User(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String)
 
     comments = db.relationship("Comment", backref="user")
+    roles = db.relationship("Role", secondary="role_identifier")
 
 
 class Ticket(db.Model, SerializerMixin):
@@ -24,7 +25,7 @@ class Ticket(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(255), nullable=False)  # at least 5 chars
-    status = db.Column(db.String, default="Opened")
+    status = db.Column(db.String, default="Opened")  # either opened or resolved
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
@@ -36,3 +37,17 @@ class Comment(db.Model, SerializerMixin):
     body = db.Column(db.String(255))
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
+class Role(db.Model, SerializerMixin):
+    __tablename__ = "roles"
+
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(10))  # either Admin or Associate
+
+
+role_identifier = db.Table(
+    "role_identifier",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("role_id", db.Intger, db.ForeignKey("roles.id")),
+)
