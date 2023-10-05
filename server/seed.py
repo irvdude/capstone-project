4809d3e39fe9112cdcd64960aea043a1860a9a81
Irvin Dinkins <irvdude44@gmail.com>
2023-10-04 20:26:42 -0400

    fully added seed logic to all models to create a complete backend replication of ticket app.

--- server/seed.py
+++ server/seed.py
@@ -21,16 +21,24 @@ if __name__ == "__main__":
         User.query.delete()
         # TicketStatus.query.delete()
         Ticket.query.delete()
-        # Comment.query.delete()
+        Comment.query.delete()
         Role.query.delete()
 
         # create 3 Roles
-        print("Creating roles")
+        print("Creating roles...")
         roles = ["Admin", "Associate", "Guest"]
         for role_name in roles:
             role = Role(role=role_name)
             db.session.add(role)
         # db.session.commit()
+
+        # create 20 random Comments
+        print("Creating Comments...")
+        comments = [fake.text() for i in range(20)]
+        for comment_text in comments:
+            created = fake.date_time_between(start_date="-1y", end_date="now")
+            comment = Comment(body=comment_text, created_at=created)
+            db.session.add(comment)
 
         # create 10 random Users
         print("Creating Users...")
@@ -48,17 +56,25 @@ if __name__ == "__main__":
             # append a random Role
             random_roles = random.sample(roles, min(len(roles), random.randint(1, 3)))
             for role_name in random_roles:
+                db.session.add(user)
                 role = Role.query.filter_by(role=role_name).first()
                 user.roles.append(role)
 
-            # append a random comment
+            # append a random Comment
+            random_comments = random.sample(
+                comments, min(len(comments), random.randint(1, 3))
+            )
+            for comment_text in random_comments:
+                db.session.add(user)
+                comment = Comment.query.filter_by(body=comment_text).first()
+                user.comments.append(comment)
 
             users.append(user)
 
         db.session.add_all(users)
 
         # create 10 Tickets
-        print("Creating tickets")
+        print("Creating tickets...")
         tickets = []
         for i in range(10):
             created = fake.date_time_between(start_date="-1y", end_date="now")
@@ -71,6 +87,15 @@ if __name__ == "__main__":
                 updated_at=updated,
             )
             tickets.append(ticket)
+
+            random_comments = random.sample(
+                comments, min(len(comments), random.randint(1, 3))
+            )
+            for comment_text in random_comments:
+                db.session.add(ticket)
+                comment = Comment.query.filter_by(body=comment_text).first()
+                ticket.comments.append(comment)
+
         db.session.add_all(tickets)
 
         # create 3 comments
