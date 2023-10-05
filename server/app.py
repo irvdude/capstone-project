124431a9f08625fdb1d435467a0429d7f400e3ca
Irvin Dinkins <irvdude44@gmail.com>
2023-10-05 18:08:19 -0400

    added alnding route to backend

--- server/app.py
+++ server/app.py
@@ -3,11 +3,12 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, jsonify
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
+
 
 # Add your model imports
 from models import User, TicketStatus, Ticket, Comment, Role
@@ -15,9 +16,17 @@ from models import User, TicketStatus, Ticket, Comment, Role
 # Views go here!
 
 
-@app.route("/")
-def index():
-    return "<h1>Project Server</h1>"
+# @app.route("/")
+# def index():
+#     return "<h1>Project Server</h1>"
+
+
+class Landing(Resource):
+    def index():
+        return "<h1>Project Server</h1>"
+
+
+api.add_resource(Landing, "/", endpoint="landing")
 
 
 if __name__ == "__main__":
